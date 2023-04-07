c48d2181688527cc8eee86a1af4f941e6040eba4
samuel-eduardo <samuellino2006@gmail.com>
2023-04-07 16:28:01 -0300

    exercício do moduo-4

--- modulo-4/js/custom.js
+++ modulo-4/js/custom.js
@@ -2,61 +2,35 @@
 // jQuery( function($){
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+      // torna o carrossel em um loop, ou seja quando chega ao fim dos produtos volta ao primeiro
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+      });
 
-    let titulos = $('h4') // tag
+
+   $('.featured-item a').addClass('btn btn-dark stretch-link');
+   $('#navbarResponsive').css('margin', '20px');
+   $('.featured-item ').mouseover(function(){
+      $(this).css('background', '#0000FF')
+      $('h4').css('color', '#ff0000')
+   })
+
+   $('.featured-item').mouseout(function(){
+     
+   })
+
    
-    let itens = $('.featured-item') // class
-    
-    let destaques = $('#featured') // id
-
-    console.log(titulos.first());
-
-    // Configuração de produtos
-
-    $('.featured-item a').addClass('btn btn-dark stretch-link');
-
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').addClass('active')
-    // $('.featured-item:first h4').removeClass('active')
-    // $('.featured-item:first h4').toggleClass('active')
-    // $('.featured-item:first h4').hide()
-    // $('.featured-item:first h4').show()
-    // $('.featured-item:first h4').fadeIn(2000)
-    // $('.featured-item:first h4').fadeOut()
-    //  $('.featured-item:first h4').css('color', '#f00')
-     
-     $('.featured-item h4').dblclick( function(){
-
-        $(this).css({
-            'color': '#f00',
-            'background': '#ff0',
-            'font-weight': '100',
-        });
-
-     });
-
-     /*
-      * Manipulação de eventos
-      */
-     $('.featured-item a').on('blur', function(event){
-
-        event.preventDefault();
-
-        alert('Produto esgotado');
-
-     })
-     
-     
-     
-     
-
-
-
-
-
-
-
-})
+});
