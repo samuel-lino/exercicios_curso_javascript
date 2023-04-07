0fa5b1569e14758b2b385252a6d54fca21ea25e2
samuel-eduardo <samuellino2006@gmail.com>
2023-04-07 16:54:03 -0300

    testando com jquery

--- modulo-4/js/custom.js
+++ modulo-4/js/custom.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
       }
       });
 
-
+   
    $('.featured-item a').addClass('btn btn-dark stretch-link');
    $('#navbarResponsive').css('margin', '20px');
    $('.featured-item ').mouseover(function(){
@@ -29,8 +29,21 @@ $(document).ready(function(){
    })
 
    $('.featured-item').mouseout(function(){
-     
+      $(this).css('background', '#ffffff')
+      $('h4').css('color', '#000000')
    })
-
+   $('.featured-item a').mouseover(function(){
+      $(this).css('color', '#008000')
+   })
+   $('.featured-item a').mouseout(function(){
+      $(this).css('color', '#ffffff')
+   })
+   $('.featured-item').append('<h5>quantidade<span class="badge text-bg-secondary">4</span></h5>')
+   $('.featured-item h5').addClass('btn btn-success')
+   $('.featured-item h5').css({
+      'margin-left': '50px',
+      'margin-top': '5px'
+      });
    
+   $('.featured-item').append('<div class="alert alert-info" role="alert">Oferta imperdivel</div>')
 });
