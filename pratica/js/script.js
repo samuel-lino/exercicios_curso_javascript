15a9c8a1b0ca0b4072440cecaf1a3e02b530ef7a
samuel-eduardo <samuellino2006@gmail.com>
2023-04-14 18:48:10 -0300

    praticando

--- pratica/js/script.js
+++ pratica/js/script.js
@@ -1,94 +1,103 @@
 
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel()
-    });
-    const emailvalido = /^[a-z0-9.]+@[a-z]+\.[a-z]+(\.[a-z]+)?$/i;
-    const telefonevalido = /^[0-9]{11}$/;
-    const nascimentovalido = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    $('.dropdown-toggle').click(function(){
-        if($('.dropdown-menu').attr('aria-labelledby') == $(this).attr('id')){
-            $('.dropdown-menu').addClass('show');
-        }    
-    });
-    $(document).click(function(event) {
-        // verifica se o clique ocorreu fora do menu suspenso ou do botão de navegação
-        if (!$(event.target).closest('.dropdown-toggle, .dropdown-menu').length) {
-            // remove a classe "show" do elemento "dropdown-menu"
-            $('.dropdown-menu').removeClass('show');
-        }
-    });
-    $('.nav-modal-open').click( function(e){
-        e.preventDefault();
-        let elem = '#' + $(this).attr('rel');
-        let conteudo = $(elem).html();
-        $('.modal-body').html(conteudo);
-        $('.modal-title').html($(this).html())
-        let mymodal = new bootstrap.Modal($('#modalId'));
-        mymodal.show();
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel()
+});
+const emailvalido = /^[a-z0-9.]+@[a-z]+\.[a-z]+(\.[a-z]+)?$/i;
+const telefonevalido = /^[0-9]{11}$/;
+const outtelefone = /\([0-9]{2}\)[0-9]{5}\-[0-9]{4}/
+const datavalida = /[0-9]{8}/
+$('.dropdown-toggle').click(function(){
+    if($('.dropdown-menu').attr('aria-labelledby') == $(this).attr('id')){
+        $('.dropdown-menu').addClass('show');
+    }    
+});
+$(document).click(function(event) {
+    // verifica se o clique ocorreu fora do menu suspenso ou do botão de navegação
+    if (!$(event.target).closest('.dropdown-toggle, .dropdown-menu').length) {
+        // remove a classe "show" do elemento "dropdown-menu"
+        $('.dropdown-menu').removeClass('show');
+    }
+});
+$('.nav-modal-open').click( function(e){
+    e.preventDefault();
+    let elem = '#' + $(this).attr('rel');
+    let conteudo = $(elem).html();
+    $('.modal-body').html(conteudo);
+    $('.modal-title').html($(this).html())
+    let mymodal = new bootstrap.Modal($('#modalId'));
+    mymodal.show();
+    
+})
 
-    })
+/*tentei assim mas não funcionou por algum motivo
+$('.nav-modal-open').click(function(){
+    let elem = $(this).attr('rel')
+    $('modal-body').html($('#' + elem).html())
+})*/
 
-    /*tentei assim mas não funcionou por algum motivo
-    $('.nav-modal-open').click(function(){
-        let elem = $(this).attr('rel')
-        $('modal-body').html($('#' + elem).html())
-    })*/
-
-    function validar(elemento){
-            if($(elemento).attr('name') == 'nome'){
-                if(elemento.value.match(/[a-z]/i)){
-                    $(elemento).removeClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
-                }
-                else{
-                    $(elemento).addClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-                }
+function validar(elemento){
+    if($(elemento).attr('name') == 'nome'){
+        if(elemento.value.match(/[a-z]/i)){
+            if(elemento.value.length > 2){
+                $(elemento).removeClass('erro');
+                $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
             }
-            else if($(elemento).attr('name') == 'email'){
-                if(elemento.value.match(emailvalido)){
-                    $(elemento).removeClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
-                }
-                else{
-                    $(elemento).addClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-                }
-            }
-            else if($(elemento).attr('name') == 'telefone'){
-                if(elemento.value.match(telefonevalido)){
-                    $(elemento).removeClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
-                }
-                else{
-                    $(elemento).addClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-                }
-            }
-            else if($(elemento).attr('name') == 'nascimento'){
-                if(elemento.value.match(nascimentovalido)){
-                    $(elemento).removeClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
-                }
-                else{
-                    $(elemento).addClass('erro');
-                    $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-                }
+            else{
+                $(elemento).addClass('erro');
+                $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
             }
             
-    };
-    $('body').on('blur', 'input' ,function(e){
-        validar(this)
-    })
-
-    $('body').on('submit', '.modal-body .form', function(e){
-        e.preventDefault();
-        const nome = $('#nome').val()
-        const email = $('#email').val()
-
-        if(nome == '' || email == ''){
-            console.log('um ou mais campos obrigatorios estão vazios')
         }
-        
-    })
+        else{
+            $(elemento).addClass('erro');
+            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
+        }
+    }
+    else if($(elemento).attr('name') == 'email'){
+        if(elemento.value.match(emailvalido)){
+            $(elemento).removeClass('erro');
+            $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
+        }
+        else{
+            $(elemento).addClass('erro');
+            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
+        }
+    }
+    else if($(elemento).attr('name') == 'nascimento'){
+        if(elemento.value != ''){
+            $(elemento).removeClass('erro');
+            $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
+        }
+        else{
+            $(elemento).addClass('erro');
+            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
+        }
+    }
     
+};
+$('body').on('blur', 'input' ,function(e){
+    validar(this)
+})
+
+$('body').on('submit', '.modal-body .form', function(e){
+    e.preventDefault();
+    let campos_obrigatorios = $(this).find('input.obrig ')
+    let tudo_ok = true
+    for(campo of campos_obrigatorios){
+        validar(campo)
+        if($(campo).hasClass('erro')){
+            tudo_ok = false
+        }
+    }
+    
+    if(tudo_ok == false){
+        console.log('um ou mais campos obrigatorios estão vazios')
+        return false
+    }
+    else{
+        $(this).submit();
+        return true
+    }
+    
+})
+
