a45ba45ee38aff44809707e0a259a2ef301796c9
samuel-eduardo <samuellino2006@gmail.com>
2023-04-15 10:01:00 -0300

    pequenas alterações

--- pratica/js/script.js
+++ pratica/js/script.js
@@ -6,18 +6,19 @@ const emailvalido = /^[a-z0-9.]+@[a-z]+\.[a-z]+(\.[a-z]+)?$/i;
 const telefonevalido = /^[0-9]{11}$/;
 const outtelefone = /\([0-9]{2}\)[0-9]{5}\-[0-9]{4}/
 const datavalida = /[0-9]{8}/
-$('.dropdown-toggle').click(function(){
+$('.dropdown-toggle').on('click', (function(e){
+    e.preventDefault();
     if($('.dropdown-menu').attr('aria-labelledby') == $(this).attr('id')){
         $('.dropdown-menu').addClass('show');
     }    
-});
-$(document).click(function(event) {
+}));
+$(document).on('click', (function(event) {
     // verifica se o clique ocorreu fora do menu suspenso ou do botão de navegação
     if (!$(event.target).closest('.dropdown-toggle, .dropdown-menu').length) {
         // remove a classe "show" do elemento "dropdown-menu"
         $('.dropdown-menu').removeClass('show');
     }
-});
+}));
 $('.nav-modal-open').click( function(e){
     e.preventDefault();
     let elem = '#' + $(this).attr('rel');
@@ -36,58 +37,59 @@ $('.nav-modal-open').click(function(){
 })*/
 
 function validar(elemento){
-    if($(elemento).attr('name') == 'nome'){
-        if(elemento.value.match(/[a-z]/i)){
-            if(elemento.value.length > 2){
+    if(elemento.value != ''){
+        if($(elemento).attr('name') == 'nome'){
+            if($(elemento).val().length > 2){
                 $(elemento).removeClass('erro');
-                $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
             }
             else{
                 $(elemento).addClass('erro');
-                $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
             }
-            
+        }
+        else if($(elemento).attr('name') == 'email'){
+            if($(elemento).val().match(emailvalido)){
+                $(elemento).removeClass('erro');
+            }
+            else{
+                $(elemento).addClass('erro');
+            }
         }
         else{
-            $(elemento).addClass('erro');
-            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-        }
-    }
-    else if($(elemento).attr('name') == 'email'){
-        if(elemento.value.match(emailvalido)){
             $(elemento).removeClass('erro');
-            $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
         }
-        else{
-            $(elemento).addClass('erro');
-            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-        }
+        
     }
-    else if($(elemento).attr('name') == 'nascimento'){
-        if(elemento.value != ''){
-            $(elemento).removeClass('erro');
-            $(elemento).parent().find('#help' + $(elemento).attr('id')).hide();
-        }
-        else{
-            $(elemento).addClass('erro');
-            $(elemento).parent().find('#help' + $(elemento).attr('id')).show();
-        }
+    else{
+        $(elemento).addClass('erro');
     }
     
 };
-$('body').on('blur', 'input' ,function(e){
+$('body').on('blur', 'input#nome' ,function(e){
+    validar(this);
+});
+$('body').on('blur', 'input#email' ,function(e){
     validar(this)
-})
+});
+$('body').on('blur', 'input#telefone' ,function(e){
+    validar(this)
+    $('#telefone').mask('(00) 00000-0000')
+});
+$('body').on('blur', 'input#data' ,function(e){
+    validar(this)
+    $('#data').mask('00/00/0000')
+});
+
 
 $('body').on('submit', '.modal-body .form', function(e){
     e.preventDefault();
     let campos_obrigatorios = $(this).find('input.obrig ')
-    let tudo_ok = true
     for(campo of campos_obrigatorios){
         validar(campo)
-        if($(campo).hasClass('erro')){
-            tudo_ok = false
-        }
+    }
+    let tudo_ok = true
+    
+    if($(this).find('input.obrig ').hasClass('erro')){
+        tudo_ok = false
     }
     
     if(tudo_ok == false){
