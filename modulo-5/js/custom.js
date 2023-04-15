f4d051f156eca80f28bb18376a3de1047628bfa7
samuel-eduardo <samuellino2006@gmail.com>
2023-04-15 10:49:07 -0300

    modificando aparencia

--- modulo-5/js/custom.js
+++ modulo-5/js/custom.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
    
    $('.featured-item a').addClass('btn btn-dark stretch-link');
    
-   $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
    // $('.featured-item:first h4').addClass('active')
@@ -28,54 +27,6 @@ $(document).ready(function(){
    // $('.featured-item:first h4').fadeOut()
    //  $('.featured-item:first h4').css('color', '#f00')
 
-   
-   $('.featured-item h4').dblclick( function(){
-      
-      $(this).css({
-         'color': '#f00',
-         'background': '#ff0',
-         'font-weight': '100',
-      });
-      
-   });
-   
-   /*
-    * Manipulação de eventos
-    */
-   $('.featured-item a').on('blur', function(event){
-      
-      event.preventDefault();
-      
-      alert('Produto esgotado');
-      
-   })
-
-   /* 
-    * Callback
-    * entendendo ações que começam ao termino de outra
-    */
-   $('.featured-item:nth(1)')
-      .hide(500, function(){
-         // este é o callback
-         console.log( $(this).find('h4').text() + ' esgotado')
-      })
-      .show(500, function(){
-         console.log( $(this).find('h4').text() + ' em estoque')
-      })
- 
-
-   /*
-    * Animações
-    */
-   const duracao = 1000 // equivalenta a 1 segundo
-
-   $('.featured-item:nth(0)')
-      .hide(duracao)
-      .show(duracao)
-      .fadeOut(duracao)
-      .fadeIn(duracao)
-      .toggle(duracao)
-      .toggle(duracao)
 
    $('#form-submit').on('click', function(e){
 
